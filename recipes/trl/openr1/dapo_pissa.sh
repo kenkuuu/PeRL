b3e443849a57c1bb1078b9aaadca7cd9756b2eef
kubota@kubota-lab-desktop <kubotake3111@gmail.com>
2026-05-08 19:41:46 +0900

    modify openr1 script

--- recipes/trl/openr1/dapo_pissa.sh
+++ recipes/trl/openr1/dapo_pissa.sh
@@ -1,14 +1,14 @@
 unset WANDB_DISABLED
-OUTPUT_DIR=outputs/grpo_pissa_qwen2_5_1_5b_$(date +%Y%m%d_%H%M%S)
+OUTPUT_DIR=outputs/dapo_pissa_qwen2_5_1_5b_$(date +%Y%m%d_%H%M%S)
 # OUTPUT_DIR=outputs/debug
 LOG_FILE=${OUTPUT_DIR}/output.log
 
 mkdir -p ${OUTPUT_DIR}
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info \
+CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info FLASHINFER_DISABLE_VERSION_CHECK=1 PYTORCH_ALLOC_CONF=expandable_segments:True \
     accelerate launch \
-    --main_process_port 29502 \
-    --config_file recipes/trl/accelerate/ds_zero2_4gpu.yaml \
+    --main_process_port 29500 \
+    --config_file recipes/trl/accelerate/ddp_4gpu.yaml \
     modules/trl/run.py train \
     --config.common.seed 42 \
     --config.common.debug false \
@@ -27,14 +27,14 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info \
     --config.training.output_dir "${OUTPUT_DIR}" \
     --config.training.run_name "${OUTPUT_DIR}" \
     --config.training.remove_unused_columns false \
-    --config.training.gradient_accumulation_steps 8 \
+    --config.training.gradient_accumulation_steps 16 \
     --config.training.num_train_epochs 1 \
     --config.training.max_completion_length 16384 \
     --config.training.num_generations 8 \
     --config.training.warmup_ratio 0.0 \
     --config.training.max_prompt_length 512 \
     --config.training.logging_steps 1 \
-    --config.training.per_device_train_batch_size 4 \
+    --config.training.per_device_train_batch_size 1 \
     --config.training.save_strategy "steps" \
     --config.training.save_steps 64 \
     --config.training.max_steps 1024 \
@@ -44,13 +44,14 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info \
     --config.training.lr_scheduler_type "constant" \
     --config.training.lr_scheduler_kwargs.min_lr_rate 0.1 \
     --config.training.vllm_mode "colocate" \
-    --config.training.vllm_gpu_memory_utilization 0.4 \
+    --config.training.vllm_gpu_memory_utilization 0.3 \
+    --config.training.gradient_checkpointing true \
     --config.training.use_liger_kernel false \
     --config.training.loss_type "dapo" \
     --config.training.report_to '["wandb"]' \
     --config.logging.trackio_space_id "Open-Tinker/Open-Tinker" \
-    --config.logging.trackio_project "grpo-full-qwen3-4b" \
-    --config.logging.wandb_project "grpo-full-qwen3-4b" \
+    --config.logging.trackio_project "dapo-full-qwen25-1_5b" \
+    --config.logging.wandb_project "dapo-full-qwen25-1_5b" \
     --config.dataset.dataset_name_or_path "open-r1/DAPO-Math-17k-Processed" \
     --config.dataset.example_numbers 1000000000 \
     &> ${LOG_FILE}
